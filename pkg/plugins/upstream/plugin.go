da7614f86451376159cb78ae6819e31413980fea
Neo Zhuo <neowiz@icloud.com>
2020-04-22 23:43:26 +0800

    fix: rewrite upstream initialize works

--- pkg/plugins/upstream/plugin.go
+++ pkg/plugins/upstream/plugin.go
@@ -2,7 +2,6 @@ package upstream
 
 import (
 	"context"
-	"time"
 
 	"github.com/blho/apexdns/pkg/types"
 
@@ -10,34 +9,8 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-type ups struct {
-	net  string
-	addr string
-	srtt float64
-}
-
-func (u *ups) srttAttenuation() {
-	u.srtt *= 0.98
-}
-
-func (u *ups) exchange(ctx context.Context, client *dns.Client, msg *dns.Msg) (r *dns.Msg, rtt time.Duration, err error) {
-	r, rtt, err = client.ExchangeContext(ctx, msg, u.addr)
-	go func(ms float64, hasError bool) {
-		if hasError {
-			u.srtt = u.srtt + 200
-			return
-		}
-		if ms > 300 {
-			ms = 300
-		}
-		u.srtt = u.srtt*0.7 + ms*0.3
-	}(float64(rtt/time.Millisecond), err != nil)
-	return r, rtt, err
-}
-
 type Plugin struct {
 	logger    *logrus.Entry
-	timeout   time.Duration
 	udpClient *dns.Client
 	tcpClient *dns.Client
 	tlsClient *dns.Client
@@ -45,26 +18,10 @@ type Plugin struct {
 }
 
 func New() *Plugin {
-	return &Plugin{
-		timeout: time.Second * 5,
-	}
+	return &Plugin{}
 }
 
-func (p *Plugin) initialize() {
-	p.udpClient = &dns.Client{
-		Net:     "udp",
-		UDPSize: dns.MaxMsgSize,
-		Timeout: p.timeout,
-	}
-	p.tcpClient = &dns.Client{
-		Net:     "tcp",
-		Timeout: p.timeout,
-	}
-	p.tlsClient = &dns.Client{
-		Net:     "tcp-tls",
-		Timeout: p.timeout,
-	}
-}
+func (p *Plugin) initialize() {}
 
 func (p *Plugin) Name() string {
 	return Name
@@ -73,17 +30,7 @@ func (p *Plugin) Name() string {
 func (p *Plugin) Tail(*types.Context) {}
 
 func (p *Plugin) Handle(ctx *types.Context) {
-	u := p.bestUpstream()
-	dnsClient := p.tcpClient
-	switch u.net {
-	case "tcp":
-		dnsClient = p.tcpClient
-	case "udp":
-		dnsClient = p.udpClient
-	case "tcp-tls":
-		dnsClient = p.tlsClient
-	}
-	response, _, err := u.exchange(context.Background(), dnsClient, ctx.GetQueryMessage())
+	response, _, err := p.bestUpstream().exchange(context.Background(), ctx.GetQueryMessage())
 	if err != nil {
 		p.logger.WithError(err).Error("Unable to exchange query")
 		ctx.AbortWithErr(err)
